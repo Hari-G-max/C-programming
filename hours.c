bc7b200be23bf564081f89169f607d888b7f79df
Hari <73341518+Hari-G-max@users.noreply.github.com>
2021-05-24 05:30:47 +0000

    Update hours.c

--- hours.c
+++ hours.c
@@ -4,16 +4,16 @@ int main()
   int i;
   for(i=1;i<=12;i++)
   {
-    printf("%2d.00 AM\n",i);
+    printf("%02d.00 AM\n",i);
     if(i==12)
     {
-      printf("%2d.00 Noon\n",i);
+      printf("%02d.00 Noon\n",i);
       for(i=1;i<=12;i++)
       {
-        printf("%2d.00 PM\n",i);
+        printf("%02d.00 PM\n",i);
         if(i==12)
         {
-          printf("%2d.00 Midnight\n",i);
+          printf("%02d.00 Midnight\n",i);
         }
       }
     }
